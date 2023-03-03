2e8e77e683fa26ba36cd1c64539816f42a2b1d54
Antoine ROBIN <antoine.robin@factorfx.com>
2023-03-03 14:11:06 +0000

    fix: script

--- agent/Windows/greenit.ps1
+++ agent/Windows/greenit.ps1
@@ -34,7 +34,7 @@ function GenerateXML
 ###
 
 #Regular expression
-$regex = "(?<Date>(?<Date_Day>[0-9]+)\/(?<Date_Month>[0-9]+)\/(?<Date_Year>[0-9]+)) (?<Time>(?<Time_Hour>[0-9]+):(?<Time_Minute>[0-9]+):(?<Time_Second>[0-9]+)): (?<Library>[\s\S]+?): (?<sensor>[_\s\S]+?): (?<Value>[0-9.,]+ [µW]+)"
+$regex = "(?<Date>(?<Date_Day>[0-9]+)\/(?<Date_Month>[0-9]+)\/(?<Date_Year>[0-9]+)) (?<Time>(?<Time_Hour>[0-9]+):(?<Time_Minute>[0-9]+):(?<Time_Second>[0-9]+)): (?<Library>[\s\S]+?): (?<sensor>[_\s\S]+?): (?<Value>[0-9.,]+ W)"
 
 #Reset variables
 $resultXML = ""
